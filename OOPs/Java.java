c110278bd671884159743e0c08a2972f53ec108e
Kumar Vikal <kumarvikal45@gmail.com>
2026-01-03 21:27:28 +0530

    newc file added

---
+++ OOPs/Java.java
@@ -0,0 +1,15 @@
+package OOPs;
+
+class Java{
+    public static void main(String[] args) {
+        System.out.println("Hello World from oop2 class!");
+        System.out.println("This is a separate class to demonstrate OOP concepts.");
+  
+        // Example of a simple calculation
+  
+        int x = 10, y = 5;
+        int sum = x + y;
+        System.out.println("The sum of x and y is: " + sum);            
+    }
+    
+}
