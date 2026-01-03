0f1af2385a5b659d2a2ea66248d42bbbf03493fa
kumarvikal45 <kumarvikal45@gmail.com>
2026-01-03 21:56:28 +0530

    Added files

---
+++ high.java
@@ -0,0 +1,8 @@
+public class high{
+   public static void main(){
+
+        System.out.println("This is the high class");
+        System.err.println("Great going!");
+
+    }
+}
---
+++ oop.java
@@ -0,0 +1,74 @@
+public class oop {
+    public static void main(String[] args0) {
+
+        /**
+         * Hello Greeting 
+         * Hii expression 
+         * Login details
+         * Face authentication
+         * Program instructions 
+         * Deteling junk files 
+         * System optimzation 
+         * User details
+         * IP Address transperancy 
+         * Network security 
+         * software update 
+         * Bug fixing 
+         * Name declaration**/
+
+        System.out.println("Hello There" + "!!");
+        System.out.println("My Name is Vikal Kumar");
+
+
+        // Int Age Declaration //
+
+        int age = 20;
+        System.out.println("My age is : " + age);
+
+        // String declaration //
+
+        
+        System.out.println("Welcome to the oop's world of java programming language ");
+        System.out.println("Let's make your skill Shine among developer's ");
+
+
+        // Int Multiplication // 
+
+        int a = 22, b = 24, cv=1144;
+        int multiply = a * cv;
+        System.out.println("The multiply of a and cv is : " + multiply );
+
+
+        // Int Addition //
+
+        int gh = b +cv;  
+         System.out.println("The addition of b and cv is : " + gh );
+
+        // Int Subtraction //
+
+         int  kv = cv - a ; 
+         System.out.println("The subtarction of cv and a is : " + kv ) ;
+
+
+        //  Int Division // 
+
+         int Intigero = cv / b ;
+         System.out.println("The division of cv and b is : " + Intigero );
+
+        //  Total Value  // 
+
+         int total = (multiply + gh + kv + Intigero);
+         System.out.println("The Total value is : " + total);
+
+
+                    //  ThankYou expressioin //
+        //  To The System User & The Group of GBU Graeter Noida //
+
+
+
+        String name = (" The Linux User & The Group of GBU Graeter Noida ");
+        System.out.println("(ThankYou Vikal Kumar !! "+ name + ")"
+
+        );
+    } 
+}
